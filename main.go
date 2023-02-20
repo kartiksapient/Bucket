adcf02641757b48291109105b7969970bb45bf23
Kartik Bansal <kartik.bansal@publicissapient.com>
2023-02-20 20:39:40 +0530

    new2

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/terraform-linters/tflint-plugin-sdk/plugin"
+	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
+	"github.com/terraform-linters/tflint-ruleset-google/google"
+	"github.com/terraform-linters/tflint-ruleset-google/project"
+	"github.com/terraform-linters/tflint-ruleset-google/rules"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		RuleSet: &google.RuleSet{
+			BuiltinRuleSet: tflint.BuiltinRuleSet{
+				Name:    "google",
+				Version: project.Version,
+				Rules:   rules.Rules,
+			},
+		},
+	})
+}
